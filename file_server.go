ea1041383bfe78bc62f62fab1c556574aa759bf4
Nils-Treuheit <75490938+Nils-Treuheit@users.noreply.github.com>
2021-07-05 22:01:40 +0200

    Update file_server.go

--- file_server.go
+++ file_server.go
@@ -2,9 +2,8 @@
 Serve is a very simple static file server in go
 Usage:
         -p="8899": port to serve on
-        -d="path/to/html/hls":    the directory of static files to host
-Navigating to http://localhost:8899 will display the index.html or directory
-listing file.
+        -d="path/to/hls":    the directory of static files to host
+Navigating to http://localhost:8899 will display the directory file listings.
 */
 package main
 
@@ -12,11 +11,20 @@ import (
         "flag"
         "log"
         "net/http"
+        "os"
 )
 
 func main() {
+        dir := "./hls"
+        args := os.Args[1:]
+        if len(args)>0 { os.Chdir(args[0]) } else {
+         fmt.Println("Warning! You did not provide the absolute path to the loacation of your hls folder.")
+         log.Println("Default to relative path...") }
+        path, err := os.Getwd()
+        if err != nil { log.Println(err) } else { dir = path+"/hls" }
+        
         port := flag.String("p", "8899", "port to serve on")
-        directory := flag.String("d", "./hls", "the directory of static file to host")
+        directory := flag.String("d", dir, "the directory of static file to host")
         flag.Parse()
 
         http.Handle("/", addHeaders(http.FileServer(http.Dir(*directory))))
