c96734addf96cbfb3624f94490ff99289eb3d83d
Nils-Treuheit <75490938+Nils-Treuheit@users.noreply.github.com>
2021-10-18 23:38:25 +0200

    bug fixes and updates

--- webpage_generator.py
+++ webpage_generator.py
@@ -3,21 +3,21 @@ import sys
 
 IP:str = sys.argv[1] if len(sys.argv) == 2 else "127.0.0.1"
 NEW_WEBSITE:str = ""
-     
+
 class MyHTMLParser(HTMLParser):
     def handle_starttag(self, tag, attrs):
         global NEW_WEBSITE, IP
         out="<"+str(tag)
         for att,value in attrs:
-            if (tag == "link" and att == 'href'): 
+            if (tag == "link" and 'rel' in attrs and att == 'href'):
                 value = 'http://'+IP+'/favicon.ico'
-            elif (tag == "div" and att == 'background'): 
+            elif (tag == "div" and att == 'background'):
                 value = 'http://'+IP+'/background.png'
-            elif (tag == "video" and att == 'poster'): 
+            elif (tag == "video" and att == 'poster'):
                 value = 'http://'+IP+'/background.png'
-            elif (tag == "source" and att == 'src'): 
+            elif (tag == "source" and att == 'src'):
                 value = 'http://'+IP+':8899/playlist.m3u8'
-            elif (att=='style'): value = '\"'+str(value)+'\"'
+            value = '\"'+str(value)+'\"'
             out+=" "+str(att)+"="+str(value)
         NEW_WEBSITE+=str(out)+">"
     def handle_endtag(self, tag):
