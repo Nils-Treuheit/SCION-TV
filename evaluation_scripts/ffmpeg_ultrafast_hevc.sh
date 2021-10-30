5d15890808f6192badf1a74e199285a3a1535f1e
Nils-Treuheit <75490938+Nils-Treuheit@users.noreply.github.com>
2021-10-30 16:48:16 +0200

    fixed crf parameter and add eval scripts

--- evaluation_scripts/ffmpeg_ultrafast_hevc.sh
+++ evaluation_scripts/ffmpeg_ultrafast_hevc.sh
@@ -1,4 +1,4 @@
-cd ../../
+cd ..
 rm -rf hls/*
 cd hls
 ffmpeg -i http://127.0.0.1:8890/bysid/772 -sn -c:v:0 libx265 -preset ultrafast -crf 28 -tune fastdecode -tune zerolatency -c:a:0 aac -b:a:0 96k -ac 2 -f hls -hls_time 6 -hls_list_size 15 -hls_flags delete_segments -hls_flags omit_endlist -hls_segment_type fmp4 -hls_fmp4_init_filename "init.mp4" -hls_segment_filename "segment_%d.m4s" "playlist.m3u8"
--- ffmpeg_scripts/vp9/ffmpeg_good_vp9.sh
+++ ffmpeg_scripts/vp9/ffmpeg_good_vp9.sh
@@ -1,4 +1,4 @@
 cd ../../
 rm -rf hls/*
 cd hls
-ffmpeg -i http://127.0.0.1:8890/bysid/772 -sn -c:v libvpx-vp9 -crf 30 -deadline good -c:a:0 aac -b:a:0 96k -ac 2 -f hls -hls_time 6 -hls_list_size 15 -hls_flags single_file -hls_flags omit_endlist -hls_segment_type fmp4 -hls_fmp4_init_filename "init.mp4" -hls_segment_filename "segment_%d.mp4" "playlist.m3u8"
+ffmpeg -i http://127.0.0.1:8890/bysid/772 -sn -c:v libvpx-vp9 -crf 28 -quality good -c:a:0 aac -b:a:0 96k -ac 2 -f hls -hls_time 6 -hls_list_size 15 -hls_flags single_file -hls_flags omit_endlist -hls_segment_type fmp4 -hls_fmp4_init_filename "init.mp4" -hls_segment_filename "segment_%d.mp4" "playlist.m3u8"
